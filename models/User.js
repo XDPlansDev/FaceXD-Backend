09803f1334b891018d40b118b16113b6e96c044c
xdplansdev <xdplansdev@gmail.com>
2025-04-06 13:01:17 -0300

    update User.js

--- models/User.js
+++ models/User.js
@@ -1,3 +1,10 @@
+// models/User.js
+// Atualização do model User para suportar o campo "username" definido pelo usuário.
+// Alterações:
+// 1. O campo "username" já está definido como required e unique.
+// 2. Removido o pre-save hook que atribuía automaticamente o email ao username,
+//    permitindo que o username seja definido no momento do registro.
+
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
@@ -55,10 +62,11 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true } // Cria campos createdAt e updatedAt automaticamente
 );
 
-// Antes de salvar, definir username como o email
-UserSchema.pre("save", function (next) {
-  this.username = this.email;
-  next();
-});
+// Removido o pre-save hook que definia username como email
+// Isso permite que o campo "username" seja definido diretamente pelo usuário durante o registro
+// UserSchema.pre("save", function (next) {
+//   this.username = this.email;
+//   next();
+// });
 
 module.exports = mongoose.model("User", UserSchema);
